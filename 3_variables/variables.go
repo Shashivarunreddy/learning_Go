3a4f97e64316305f94d4007880b63f4bea9745e5
Shashivarunreddy <shashivarun6154@gmail.com>
2025-09-07 18:53:28 +0000

    variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	//integer
+	var num int = 1
+	var num2 = 2
+	num3 := 3
+
+	fmt.Println(num)
+	fmt.Println(num2)	
+	fmt.Println(num3)
+
+	//float
+	var numf float32 = 1.5
+	var numf2 = 2.5
+	numf3 := 3.5
+	
+	fmt.Println(numf)
+	fmt.Println(numf2)
+	fmt.Println(numf3)
+
+	//sting
+	var str string = "hello"
+	var str2 = "hello world"
+	str3 := "hello world !!!"
+	
+	fmt.Println(str)
+	fmt.Println(str2)
+	fmt.Println(str3)
+
+	//boolean
+	var b1 bool = true
+	var b2 = false
+	b3 := true
+
+				
+	fmt.Println(b1)
+	fmt.Println(b2)
+	fmt.Println(b3)
+			
+
+}
