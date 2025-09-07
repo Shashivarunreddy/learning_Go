bec7ef13e67645bd72e3e9d50840c10f7099432a
Shashivarunreddy <shashivarun6154@gmail.com>
2025-09-07 18:28:24 +0000

    simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    //normal values
+	//integer
+
+	fmt.Println(1)
+	fmt.Println(1+1)
+	fmt.Println(2-1)
+
+	//float
+	fmt.Println(1.5)
+	fmt.Println(1.5+1.5)
+	fmt.Println(2.5-1.5)
+	fmt.Println(2.5*2)
+	fmt.Println(2.5/2)
+
+	//string
+	fmt.Println("hello")
+
+	//boolean
+	fmt.Println(true)
+	fmt.Println(false) 
+
+}
