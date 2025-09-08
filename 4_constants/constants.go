4cc138ed5751953cc6347916dffe379cc14610ca
Shashivarunreddy <shashivarun6154@gmail.com>
2025-09-08 15:11:59 +0000

    constants

---
+++ 4_constants/constants.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const name2 string = "Gophersss"
+const pi = 3.14
+func main() {
+	const name string = "Gopher"
+	const age = 10
+
+
+	const(
+		a = 1
+		name3 = "Gopher3"
+		age3 = 20
+	)
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(name2)
+	fmt.Println(pi)
+	fmt.Println(a)
+	fmt.Println(name3)
+	fmt.Println(age3)
+}
