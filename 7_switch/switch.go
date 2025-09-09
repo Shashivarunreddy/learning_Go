a3ea322c868121ca2e59797d08e92d8fe0c6ae7b
Shashivarunreddy <shashivarun6154@gmail.com>
2025-09-09 10:03:40 +0000

    switch

---
+++ 7_switch/switch.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	default:
+		fmt.Println("unknown number")
+	}
+
+	//multiple condition switch
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("it's weekend")
+	default:
+		fmt.Println("it's workday")
+	}
+
+
+
+	// type switch
+	whoAmI := func(i interface{}) {
+		switch i.(type) {
+		case int:
+			fmt.Println("its an integer")
+		case string:
+			fmt.Println("its a string")
+		case bool:
+			fmt.Println("its a boolean")
+		default:
+			fmt.Println("other")
+		}
+	}
+
+	whoAmI(true)
+	whoAmI(1)
+	whoAmI("hello")
+	whoAmI(2.3)
+}
